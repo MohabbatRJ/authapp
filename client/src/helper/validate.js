9d0e9fd656ef5f8f33307ce8eed1b72f850c37d7
MohabbatRJ <MohabbatRJ911@gmail.com>
2024-01-20 23:53:10 +0500

    register ui and img convert also formik validation with toast

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast'
 export async function usernameValidate(values) {
     const errors = usernameVerify({}, values);
 
-    return errors
+    return errors;
 }
 
 // validate password page
@@ -26,6 +26,17 @@ export async function resetPasswordValidate(values) {
 }
 
 
+// register page
+export async function registerValidation(values){
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
+
+// --------------------------- 
 // validate username
 function usernameVerify(error = {}, values) {
     if (!values.username) {
@@ -59,23 +70,19 @@ function passwordVerify(errors = {}, values) {
     return errors;
 }
 
-// validate Password
-function resetPassword(errors = {}, values) {
+// validate email
 
-    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-
-    if (!values.password) {
-        errors.password = toast.error('Password Required...!');
+function emailVerify(error = {}, values){
+    if (!values.email) {
+        error.email = toast.error('Email Required...!');
     }
-    else if (values.password.includes(" ")) {
-        errors.password = toast.error('Wrong Password...!');
-    }
-    else if (values.password.length < 6) {
-        errors.password = toast.error('Password must be 6 character long...!');
-    }
-    else if (!specialChars.test(values.password)) {
-        errors.password = toast.error('Password must have special character...!');
+    else if (values.email.includes(" ")) {
+        error.email = toast.error('Wrong Email...!');
     }
 
-    return errors;
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        error.email = toast.error('Invalid Email Address...!');
+    }
+
+    return error;
 }
