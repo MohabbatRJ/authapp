605e979de1fb5dbbbf37df70deeac7fec2f3efac
MohabbatRJ <MohabbatRJ911@gmail.com>
2024-01-20 17:25:20 +0500

    Password component ui with toast error

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -7,6 +7,12 @@ export async function usernameValidate(values) {
     return errors
 }
 
+// validate password page
+export async function passwordValidate(values) {
+    const errors = passwordVerify({}, values);
+
+    return errors
+}
 
 // validate username
 function usernameVerify(error = {}, values) {
@@ -18,4 +24,25 @@ function usernameVerify(error = {}, values) {
     }
 
     return error;
+}
+
+// validate Password
+function passwordVerify(errors = {}, values) {
+
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if (!values.password) {
+        errors.password = toast.error('Password Required...!');
+    }
+    else if (values.password.includes(" ")) {
+        errors.password = toast.error('Wrong Password...!');
+    }
+    else if(values.password.length < 6){
+        errors.password = toast.error('Password must be 6 character long...!');
+    }
+    else if (!specialChars.test(values.password)) {
+        errors.password = toast.error('Password must have special character...!');
+    }
+
+    return errors;
 }
