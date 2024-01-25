604c50a7d2a977113141dcb659a73b4ccc3b16c0
MohabbatRJ <MohabbatRJ911@gmail.com>
2024-01-26 00:29:27 +0500

    registrer controller

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,4 +1,5 @@
-
+import UserModel from "../models/User.model.js";
+import bcrypt from 'bcrypt';
 
 /** POST: http://localhost:8080/api/register 
  * @param : {
@@ -13,7 +14,68 @@
 }
 */
 export async function register(req, res) {
-    res.json('register route');
+    try {
+        const { username, password, profile, email } = req.body;
+
+        let existUsername;
+        let existEmail;
+
+        // Check for existing user
+        try {
+            existUsername = await UserModel.findOne({ username }).exec();
+
+            if (existUsername) {
+                throw new Error("Please use a unique username");
+            }
+        }
+        catch (error) {
+            console.error(error.message);
+            return res.status(500).send({ error: error.message });
+        }
+
+        // Check for existing email
+        try {
+            existEmail = await UserModel.findOne({ email }).exec();
+
+            if (existEmail) {
+                throw new Error("Please use a unique email");
+            }
+        }
+        catch (error) {
+            console.error(error.message);
+            return res.status(500).send({ error: error.message });
+        }
+
+        // Continue with your code if both username and email are unique
+        try {
+            if (password) {
+                const hashedPassword = await bcrypt.hash(password, 10);
+
+                const user = new UserModel({
+                    username,
+                    password: hashedPassword,
+                    profile: profile || '',
+                    email,
+                });
+
+                // Return save result as a response
+                const result = await user.save();
+                return res.status(201).send({ msg: "User Register Successfully", result });
+            }
+            else {
+                return res.status(201).send({ msg: "Please user valid password", result });
+            }
+        }
+        catch (error) {
+            console.error(error.message);
+            return res.status(500).send({ error: "Unable to hash password or save user" });
+        }
+
+    }
+    catch (error) {
+        console.error(error.message);
+        return res.status(500).send({ error: "Internal Server Error" });
+    }
 }
 
 
