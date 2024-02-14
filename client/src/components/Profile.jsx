472b6dc1311941ebc768a61505b84b9f8af63fbd
MohabbatRJ <MohabbatRJ911@gmail.com>
2024-02-15 01:32:00 +0500

    profile component setup update data and logout

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,32 +1,43 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import avatar from '../assets/profile.png'
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import { useFormik } from 'formik'
-import { registerValidation } from "../helper/validate"
+import { profileValidation } from "../helper/validate"
 import { convertToBase64 } from "../helper/convert"
 import { useState } from "react"
+import useFetch from "../hooks/fetch.hook"
 
 import styles from '../styles/Username.module.css'
 import extend from '../styles/Profile.module.css'
+import { updateUser } from "../helper/helper"
 
 export default function Profile() {
 
   const [file, setFile] = useState();
+  const [{ isLoading, apiData, serverError }] = useFetch()
+  const navigate = useNavigate();
 
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      mobile: '',
-      address: '',
+      firstName: apiData?.firstName || '',
+      lastName: apiData?.lastName || '',
+      email: apiData?.email || '',
+      mobile: apiData?.mobile || '',
+      address: apiData?.address || '',
     },
-    validate: registerValidation,
+    enableReinitialize: true,
+    validate: profileValidation,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
-      values = await Object.assign(values, { profile: file || '' })
-      console.log(values)
+      values = await Object.assign(values, { profile: file || apiData?.profile || '' })
+      let updatePromise = updateUser(values);
+
+      toast.promise(updatePromise, {
+        loading: 'Updating...!',
+        success: <b>Update Successfully...!</b>,
+        error: <b>Could not Update!</b>
+      });
     }
   })
 
@@ -34,8 +45,16 @@ export default function Profile() {
   const onUpload = async (e) => {
     const base64 = await convertToBase64(e.target.files[0]);
     setFile(base64);
-
   }
+
+  // logout handler function
+  function userLogout() {
+    localStorage.removeItem('token');
+    navigate('/');
+  }
+
+  if (isLoading) return <h1 className="text-2xl font-bold">isLoading</h1>
+  if (serverError) return <h1 className="text-xl text-red-500">{serverError.message}</h1>
 
   return (
     <div className="container mx-auto">
@@ -53,7 +72,7 @@ export default function Profile() {
           <form onSubmit={formik.handleSubmit} className="py-1">
             <div className="profile flex justify-center py-4">
               <label htmlFor="profile">
-                <img src={file || avatar} alt="avatar" className={`${styles.profile_img} ${extend.profile_img}`} />
+                <img src={apiData?.profile || file || avatar} alt="avatar" className={`${styles.profile_img} ${extend.profile_img}`} />
               </label>
 
               <input type="file" id="profile" name="profile" onChange={onUpload} />
@@ -73,11 +92,11 @@ export default function Profile() {
               <div className={extend.name}>
                 <input type="text" placeholder="Address" className={`${styles.textbox} ${extend.textbox} w-full`} {...formik.getFieldProps('address')} />
               </div>
-                <button className={`${styles.btn} ${extend.textbox}`} type="submit">Register</button>
+              <button className={`${styles.btn} ${extend.textbox}`} type="submit">Update</button>
             </div>
 
             <div className="text-center py-4">
-              <span className="text-gray-500">come back later? <Link className={styles.btn_link} to="/">Logout</Link> </span>
+              <span className="text-gray-500">come back later? <Link onClick={userLogout} className={styles.btn_link} to="/">Logout</Link> </span>
             </div>
           </form>
         </div>
